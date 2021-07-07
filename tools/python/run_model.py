7dcd685bbcb0e92d6e272b27526d37d521968791
x.wang <tsinghuawangx@gmail.com>
2021-07-07 21:06:13 +0800

    remove multi_thread in Python

--- tools/python/run_model.py
+++ tools/python/run_model.py
@@ -85,23 +85,11 @@ def run_models(flags, args):
 
 def run_models_for_device(flags, args, dev):
     conf = config_parser.parse(flags.config)
-    ps = []
     for name, model_conf in conf["models"].items():
         if not flags.model_name or name == flags.model_name:
-            p = Process(target=model_process, args=(flags, args, dev, name, model_conf))
-            p.start()
-            ps.append(p)
-            # MaceLogger.info("Run model %s" % name)
-            # model_conf = config_parser.normalize_model_config(model_conf)
-            # run_model_for_device(flags, args, dev, name, model_conf)
-    for p in ps:
-        p.join()
-            
-def model_process(flags, args, dev, name, model_conf):
-    MaceLogger.info("Run model %s" % name)
-    MaceLogger.info("Current process is : %s" % os.getpid())
-    model_conf = config_parser.normalize_model_config(model_conf)
-    run_model_for_device(flags, args, dev, name, model_conf)
+            MaceLogger.info("Run model %s" % name)
+            model_conf = config_parser.normalize_model_config(model_conf)
+            run_model_for_device(flags, args, dev, name, model_conf)
 
 def run_model_for_device(flags, args, dev, model_name, model_conf):
     target_abi = flags.target_abi
