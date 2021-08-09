d5d62a5d5f62456a5f774d2e3ba78f6fcc5579a2
x.wang <tsinghuawangx@gmail.com>
2021-08-09 20:02:37 +0800

    remove Eltwise fallback

--- mace/core/net_optimizer.cc
+++ mace/core/net_optimizer.cc
@@ -27,7 +27,8 @@ RuntimeType NetOptimizer::SelectBestRuntime(
       "Conv2D", "DepthwiseConv2d", "Deconv2D", "DepthwiseDeconv2d",
       "FullyConnected"
   };
-  static const std::set<std::string> kCpuFallbackOps = {"Pooling", "Pad", "Eltwise"};
+  // static const std::set<std::string> kCpuFallbackOps = {"Pooling", "Pad", "Eltwise"};
+  static const std::set<std::string> kCpuFallbackOps = {"Pooling", "Pad"};
   // CPU is the runtime to fall back
   RuntimeType best_runtime = RuntimeType::RT_CPU;
   if (kCpuFallbackOps.count(op_def->type()) == 1) {
